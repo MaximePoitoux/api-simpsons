8474addbf69a75370f5435421e4f8f61e2be844b
Maxime Poitoux <poitouxmaxime@hotmail.fr>
2020-04-18 14:07:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import QuoteCards from './components/QuoteCard'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const sampleQuote = [
+  {
+    image: 'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939',
+    quote: "Facts are meaningless. You could use facts to prove anything that's even remotely true.",
+    character: 'Homer Simpson',
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: sampleQuote[0]
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+
+  getQuote() {
+    axios.get('https://thesimpsonsquoteapi.glitch.me/quotes')
+      .then(response => response.data)
+      .then(data => {
+        this.setState({
+          quotes: data[0],
+        });
+    });
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <QuoteCards quotes={this.state.quotes} />
+        <button type="button" onClick={this.getQuote}>Get quote</button>
+      </div>
+    );
+  }
 }
 
 export default App;
